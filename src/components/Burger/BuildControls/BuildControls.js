56b855607c5c3f5985a6918842259bd98fe42be5
takunnithan <takunnithan@gmail.com>
2018-02-28 23:40:16 +0530

    Bug fix

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -12,7 +12,7 @@ const controls = [
 ]
 
 const buildControls = (props) => (
-    <div className={classes.buildControls}>
+    <div className={classes.BuildControls}>
         {controls.map(ctrl => (
             <BuildControl key={ctrl.label} label={ctrl.label} />
         ))}
