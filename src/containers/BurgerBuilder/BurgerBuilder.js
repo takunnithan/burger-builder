c7b5c936bda57ed756c9de44dc1ec58c5d2d13f1
takunnithan <takunnithan@gmail.com>
2018-03-02 00:23:06 +0530

    Added Button Component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -35,6 +35,10 @@ class BurgerBuilder extends Component{
         this.setState({purchasing:false});
     }
 
+    purchaseContinueHandler = () => {
+        alert('Continue!');
+    }
+
     updatePurchaseState (ingredients) {
         const sum = Object.keys(ingredients)
         .map(igKey => {
@@ -82,7 +86,10 @@ class BurgerBuilder extends Component{
         return (
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler} >
-                    <OrderSummary ingredients={this.state.ingredients} />
+                    <OrderSummary 
+                        ingredients={this.state.ingredients} 
+                        purchaseCancel={this.purchaseCancelHandler}
+                        purchaseContinue={this.purchaseContinueHandler} />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls 
