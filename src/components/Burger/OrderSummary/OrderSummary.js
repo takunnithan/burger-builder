61c0ee1a3eb49b528ba4b4fa5997c95e23866b1a
takunnithan <takunnithan@gmail.com>
2018-03-03 12:56:59 +0530

    Added total price to the Modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -18,6 +18,7 @@ const orderSummary = (props) => {
             <ul>
                 {ingredientSummary}
             </ul>
+            <p><strong>Price: {props.price.toFixed(2)}</strong></p>
             <p>Continue to checkout?</p>
             <Button btnType='Danger' clicked={props.purchaseCancel}>Cancel</Button>
             <Button btnType='Success' clicked={props.purchaseContinue}>Continue</Button>
