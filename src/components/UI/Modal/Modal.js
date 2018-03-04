3b7ed35c9f20d39cea637269f4b9181dbd978390
takunnithan <takunnithan@gmail.com>
2018-03-04 13:49:40 +0530

    1. Added Hamburger menu for DrawerToggle 2. Bug fix on Modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -10,7 +10,8 @@ const modal = (props) => (
         <div className={classes.Modal}
          style={{
              transition: props.show ? 'translateY(0)': 'translateY(-100vh)',
-             opacity: props.show ? '1' : '0'
+             opacity: props.show ? '1' : '0',
+             display: props.show ? 'block' : 'none'
          }}
         >
             {props.children}
